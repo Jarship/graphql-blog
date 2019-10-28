f2f5630bb75ecd096bba233ec71498426a3dbc40
Ubuntu <ubuntu@ip-172-31-62-237.ec2.internal>
2019-10-28 18:49:48 +0000

    Add production changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: './graphql-blog/src/schema.graphql',
   resolvers,
   context: request => {
     return {
