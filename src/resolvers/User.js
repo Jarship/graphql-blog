4c2cbae112a7db437f12ee57f214c66dc0a46b42
Jarship <JohnArthurBishop@gmail.com>
2019-11-04 20:23:38 -0500

    Add profile photos, and check for unauthenticated users

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,8 +1,25 @@
+const fetch = require('isomorphic-unfetch');
+
 async function invitations (parent, args, context) {
+  if(!parent.id) return "";
   const invites = await context.prisma.user({ id: parent.id }).invitations();
   return invites.map(invite => invite.token);
 };
 
+async function photo (parent, args, context) {
+  if(!parent.id) return "";
+  const user = await context.prisma.user({ id: parent.id });
+  if (!user.photo) {
+    const photo = await fetch(`https://ui-avatars.com/api/?rounded=true&name=${user.name}`, {
+      method: 'GET'
+    });
+    return photo.url;
+  } else {
+    return user.photo;
+  }
+}
+
 module.exports = {
   invitations,
+  photo,
 };
